2838c09f6289281952f34ae780092702f03061b2
free-flow-code <wildmuskaria8@gmail.com>
2023-09-14 14:51:43 +0300

    Add DialogFlow to VK bot.

--- vk_bot.py
+++ vk_bot.py
@@ -1,13 +1,14 @@
 import random
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
+from google_cloud_funcs import create_api_key, detect_intent_text
 from environs import Env
 
 
-def echo(event, vk_api):
+def handle_text(event, vk_api, project_id):
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=detect_intent_text(project_id, event.user_id, event.text, 'ru-RU'),
         random_id=random.randint(1,1000)
     )
 
@@ -15,13 +16,14 @@ def echo(event, vk_api):
 def main():
     env = Env()
     env.read_env()
+    project_id = env.str('PROJECT_ID')
     vk_session = vk.VkApi(token=env.str('VK_TOKEN'))
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            handle_text(event, vk_api, project_id)
 
 
 if __name__ == '__main__':
